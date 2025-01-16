f51532cbc79f8b2ab1aee9e226ea001813d4bde0
AzLegendUp <davidfreddy.c.c@gmail.com>
2025-01-16 16:18:14 -0500

    Add project files.

---
+++ BestStoreMVC/Controllers/ProductsController.cs
@@ -0,0 +1,161 @@
+﻿using BestStoreMVC.Models;
+using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace BestStoreMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly IWebHostEnvironment enviroment;
+
+        public ProductsController(ApplicationDbContext context, IWebHostEnvironment enviroment)
+        {
+            this.context = context;
+            this.enviroment = enviroment;
+        }
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p => p.Id).ToList();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ProductDto productDto)
+        {
+            if (productDto.ImageFile == null)
+            {
+                ModelState.AddModelError("ImageFile", "The image file is required");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return View(productDto);
+            }
+
+            //Save the image file 
+            string newFileName = DateTime.Now.ToString("yyyMMddHHmmssfff");
+            newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+
+            string imageFullPath = enviroment.WebRootPath + "/products/" + newFileName;
+            using (var stream = System.IO.File.Create(imageFullPath))
+            {
+                productDto.ImageFile.CopyTo(stream);
+            }
+
+            //Save the product IN THE DATEBASE
+            Product product = new Product
+            {
+                Name = productDto.Name,
+                Brand = productDto.Brand,
+                Category = productDto.Category,
+                Price = productDto.Price,
+                Description = productDto.Description,
+                ImageFileName = newFileName,
+                CreateAt = DateTime.Now
+            };
+
+            context.Products.Add(product);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Products");
+
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+
+            //Create a ProductDto object
+            ProductDto productDto = new ProductDto
+            {
+                Name = product.Name,
+                Brand = product.Brand,
+                Category = product.Category,
+                Price = product.Price,
+                Description = product.Description
+            };
+
+            ViewData["ProductId"] = product.Id;
+            ViewData["ImageFileName"] = product.ImageFileName;
+            ViewData["CreateAt"] = product.CreateAt.ToString("MM/dd/yyyy");
+
+            return View(productDto);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, ProductDto productDto)
+        {
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+            if (!ModelState.IsValid)
+            {
+                ViewData["ProductId"] = product.Id;
+                ViewData["ImageFileName"] = product.ImageFileName;
+                ViewData["CreateAt"] = product.CreateAt.ToString("MM/dd/yyyy");
+                return View(productDto);
+            }
+            //Update the image file if the user selected a new image file
+            string newFileName = product.ImageFileName;
+            if (productDto.ImageFile != null)
+            {
+                newFileName = DateTime.Now.ToString("yyyMMddHHmmssfff");
+                newFileName += Path.GetExtension(productDto.ImageFile!.FileName);
+
+                string imageFullPath = enviroment.WebRootPath + "/products/" + newFileName;
+                using (var stream = System.IO.File.Create(imageFullPath))
+                {
+                    productDto.ImageFile.CopyTo(stream);
+                }
+
+                //Delete the old image file
+                string oldImagePath = enviroment.WebRootPath + "/products/" + product.ImageFileName;
+                System.IO.File.Exists(oldImagePath);
+
+
+
+            }
+
+            //Update the product in the database
+            product.Name = productDto.Name;
+            product.Brand = productDto.Brand;
+            product.Category = productDto.Category;
+            product.Price = productDto.Price;
+            product.Description = productDto.Description;
+            product.ImageFileName = newFileName;
+
+            context.SaveChanges();
+
+            return RedirectToAction("Index", "Products");
+
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var product = context.Products.Find(id);
+            if (product == null)
+            {
+                return RedirectToAction("Index", "Products");
+            }
+            
+            string imageFullPath = enviroment.WebRootPath + "/products/" + product.ImageFileName;
+            System.IO.File.Delete(imageFullPath);
+            
+            context.Products.Remove(product);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Products");
+        }
+    }
+}
---
+++ BestStoreMVC/Migrations/20250116152355_UpdatePriceType.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BestStoreMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePriceType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(16,2)",
+                precision: 16,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldPrecision: 16,
+                oldScale: 2);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Price",
+                table: "Products",
+                type: "nvarchar(max)",
+                precision: 16,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(16,2)",
+                oldPrecision: 16,
+                oldScale: 2);
+        }
+    }
+}
